afa6f56ed0d58e44b907b348016e76eda0dc2160
Pekillen <piotr.kwiatkowski@gmail.com>
2022-03-10 17:37:33 +0100

    Toggle Button Level

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {displayBio: false };
+
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    }
+    toggleDisplayBio(){
+        this.setState({displayBio: !this.state.displayBio})
+    }
+    render() {            
+        return(
+            <div>
+                <h1>Velkommen!</h1>
+                <p>My name is Peter. I am aspiring software engineer.</p>
+                <p>Among my interests there are: Music, Languages and Science, but for some time now I am hooked up on IT!</p>
+                {
+                  this.state.displayBio ? (
+                       <div>
+                       <p>I recently moved to Norway with my girlfriend.</p>
+                        <p>I am actively learning Norwegian Language, and if you say something in Norwegian, there is a high probability I will be able to understand.</p>
+                        <p>I like spending time around people, because they always have something interesting to share.</p>
+                        <p>And I always want to learn.</p>
+                        <div>
+                            <button onClick={this.toggleDisplayBio}>Read Less</button>
+                        </div>
+                  </div> ) : (
+                      <div>
+                          <button onClick={this.toggleDisplayBio}>Read More</button>
+                      </div>
+                  )
+                }
+            </div>
+        )
+    }
 }
-
 export default App;

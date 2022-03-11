775861cc4e1ecff720465033aefd194c9760c3c7
Pekillen <piotr.kwiatkowski@gmail.com>
2022-03-11 10:56:59 +0100

    Before the storm - Chapter 1

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     render() {            
         return(
             <div>
-                <img src={profile} alt='profile' className='profile' />
+                <img src={profile} alt= 'profile' className= 'profile' />
                 <h1>Velkommen!</h1>
                 <p>My name is Peter. I am an aspiring software engineer.</p>
                 <p>Among my interests there are: Music, Languages and Science, but for some time now I am hooked up on IT!</p>
@@ -24,7 +24,7 @@ class App extends Component {
                        <p>I recently moved to Norway with my girlfriend.</p>
                         <p>I am actively learning Norwegian Language, and if you say something in Norwegian, there is a high probability I will be able to understand.</p>
                         <p>I like spending time around people, because they always have something interesting to share.</p>
-                        <p>And I always want to learn.</p>
+                        <p>And I am always eager to learn.</p>
                         <div>
                             <button onClick={this.toggleDisplayBio}>Read Less</button>
                         </div>

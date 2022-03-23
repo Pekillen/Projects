647c14f371d38ce5e8d7c8ab61e88c39f863d621
Pekillen <piotr.kwiatkowski@gmail.com>
2022-03-23 14:11:46 +0100

    Completed before styling

--- e-commerce/src/components/CheckoutForm/Checkout/Checkout.jsx
+++ e-commerce/src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button, CssBaseline } from '@material-ui/core';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
+import { Link, useNavigate } from 'react-router-dom';
 
 import useStyles from './styles';
 import { commerce } from '../../../lib/commerce';
@@ -13,6 +14,8 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
     const [activeStep, setActiveStep] = useState(0);
     const [shippingData, setShippingData] = useState({});
     const [checkoutToken, setCheckoutToken] = useState(null);
+    const [isFinished, setIsFinished] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const generateToken = async () => {
@@ -21,7 +24,7 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
 
                 setCheckoutToken(token);
             } catch (error) {
-                console.log(error);
+                navigate('/');
             }
         }
 
@@ -37,20 +40,60 @@ const Checkout = ({ cart, order, onCaptureCheckout, error }) => {
         nextStep();
     }
 
-    const Confirmation = () => (
-        <div>
-            Confirmation
+    const timeout = () => {
+        setTimeout(() => {
+            setIsFinished(true)
+        }, 10000);
+    }
+
+    let Confirmation = () => (order.customer ? (
+        <>
+          <div>
+            <Typography variant="h5">Thank you for your purchase, {order.customer.firstname} {order.customer.lastname}!</Typography>
+            <Divider className={classes.divider} />
+            <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
+          </div>
+          <br />
+          <Button component={Link} variant="outlined" type="button" to="/">Back to home</Button>
+        </>
+      ) : isFinished ? (
+          <>
+            <div>
+                <Typography variant="h5">Thank you for your purchase!</Typography>
+                <br />
+                <Typography variant="p">Your Transaction was completed with setTimeout function, since the card number you provided was invalid.</Typography>
+                <br />
+                <Typography variant="p">See you later!</Typography>
+                <Divider className={classes.divider} />                
+            </div>
+            <br />
+            <Button component={Link} variant="outlined" type="button" to="/">Back to home</Button>          
+          </>
+      ) : (
+        <div className={classes.spinner}>
+          <CircularProgress />
         </div>
-    );
+      ));
+    
+      if (error) {
+        Confirmation = () => (
+          <>
+            <Typography variant="h5">Error: {error}</Typography>
+            <br />
+            <Button component={Link} variant="outlined" type="button" to="/">Back to home</Button>
+          </>
+        );
+      }
 
     const Form = () => (activeStep === 0 
         ? <AddressForm checkoutToken={checkoutToken} next={next} />
-        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep} onCaptureCheckout={onCaptureCheckout} nextStep={nextStep} />);
+        : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} backStep={backStep} onCaptureCheckout={onCaptureCheckout} nextStep={nextStep} timeout={timeout} />);
 
     
 
   return (
     <>
+    <CssBaseline />
         <div className={classes.toolbar} /> 
         <main className={classes.layout}>
             <Paper className={classes.paper}>
--- e-commerce/src/components/CheckoutForm/PaymentForm.jsx
+++ e-commerce/src/components/CheckoutForm/PaymentForm.jsx
@@ -7,7 +7,7 @@ import Review from './Review';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
 
-const PaymentForm = ({ shippingData, checkoutToken, backStep, onCaptureCheckout, nextStep }) => {
+const PaymentForm = ({ shippingData, checkoutToken, backStep, onCaptureCheckout, nextStep, timeout }) => {
     const handleSubmit = async (event, elements, stripe) => {
         event.preventDefault();
 
@@ -29,6 +29,8 @@ const PaymentForm = ({ shippingData, checkoutToken, backStep, onCaptureCheckout,
             }
 
             onCaptureCheckout(checkoutToken.id, orderData);
+
+            timeout();
 
             nextStep();
         }

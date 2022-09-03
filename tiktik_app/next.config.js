5645567a2971b965fd17373b638d87f10a7b3088
Pekillen <piotr.kwiatkowski@gmail.com>
2022-09-03 14:47:45 +0200

    Main funcionality ready

--- tiktik_app/next.config.js
+++ tiktik_app/next.config.js
@@ -1,7 +1,17 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: [
+      'i.stack.imgur.com',
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com',
+      'images.photowall.com',      
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

38846598e61f0d0b182d5325c1420fe17f0cae69
Pekillen <piotrk13@wp.pl>
2023-03-24 10:21:52 +0100

    .node-version added

--- memories-project/server/index.js
+++ memories-project/server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
